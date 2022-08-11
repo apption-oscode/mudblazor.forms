8db76a66aa79873e1598888415f4c03fada54d9b
Nabeel Bader <nbader@apption.com>
2022-08-11 12:25:59 -0400

    added interactive dropdowns v2

---
+++ Elemental.Documentation/Samples/StarshipHierarchy.cs
@@ -0,0 +1,43 @@
+﻿using MudBlazor.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudForms.Documentation.Samples
+{
+    public class StarshipHierarchy
+    {
+        [AeFormIgnore]
+        private int ID { get; set; } = 1;        
+
+        [AeFormCategory("Hierarchy")]
+        [Required]
+        [MudForm("Starship Sector", IsDropDown =true)]
+        public StarshipLevel StarshipSector { get; set; }
+
+
+        [AeFormCategory("Hierarchy")]
+        [Required]
+        [MudForm("Starship Branch", IsDropDown = true)]
+        public StarshipLevel StarshipBranch { get; set; }
+
+        [AeFormCategory("Hierarchy")]
+        [Required]
+        [MudForm("Starship Branch", IsDropDown = true)]
+        public StarshipLevel StarshipDivision { get; set; }
+
+
+
+
+    }
+
+    public class StarshipLevel
+    { 
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string LevelName { get; set; }        
+    }
+}
