a283e2ef4edd74c5b420e2cd1fd4314b49ea0ed1
Erik <erik@apption.com>
2024-10-21 17:10:50 -0400

    - removed elemental legacy code - fixed mudblazor version

--- Elemental.FormBuilder/wwwroot/js/highlight-init.js
+++
@@ -1,5 +0,0 @@
-﻿window.initHighlight = () => {
-    document.querySelectorAll('pre code').forEach((block) => {
-        hljs.highlightBlock(block);
-    });
-}
--- MudForms.Documentation.Wasm/Program.cs
+++ MudForms.Documentation.Wasm/Program.cs
@@ -18,8 +18,7 @@ namespace MudForms.Documentation.Wasm
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.Services.AddSingleton<IHostEnvironment,WasmHostEnvironment>();
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddElemental();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });            
             await builder.Build().RunAsync();
         }
     }
