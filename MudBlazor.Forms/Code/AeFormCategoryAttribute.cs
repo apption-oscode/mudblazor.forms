d88167a9e545dae70c0da909bb17bc00152cc7fc
Erik <erik@apption.com>
2024-10-22 17:21:08 -0400

    fixed xml doc generation

--- MudBlazor.Forms/Code/AeFormCategoryAttribute.cs
+++ MudBlazor.Forms/Code/AeFormCategoryAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace MudBlazor.Forms
 {
+    /// <summary>
+    /// Attribute to define the category of a property in a form.
+    /// </summary>
     public class AeFormCategoryAttribute : Attribute
     {
         public AeFormCategoryAttribute(string category, int categoryOrder = 0)
@@ -10,7 +13,14 @@ namespace MudBlazor.Forms
             CategoryOrder = categoryOrder;
         }
 
+        /// <summary>
+        /// The category of the property.
+        /// </summary>
         public string Category { get; }
+
+        /// <summary>
+        /// The order of the category.
+        /// </summary>
         public int CategoryOrder { get; set; }
     }
 }
